4f60a1b48747cd104f257289814b999a24691456
Jérémy TOUZY <j.touzy@qualiac.com>
2015-08-16 10:47:49 +0200

    Externalisation depuis cv-api

---
+++ src/main/java/com/jtouzy/cv/model/dao/SeasonDAO.java
@@ -0,0 +1,21 @@
+package com.jtouzy.cv.model.dao;
+
+import com.jtouzy.cv.model.classes.Season;
+import com.jtouzy.dao.errors.DAOException;
+import com.jtouzy.dao.impl.AbstractSingleIdentifierDAO;
+
+/**
+ * Implémentation d'un DAO pour le modèle "Season"
+ * @author jtouzy
+ */
+public class SeasonDAO extends AbstractSingleIdentifierDAO<Season> {
+	/**
+	 * Constructeur du DAO 
+	 * @param daoClass Classe modèle gérée par le DAO (Season)
+	 * @throws DAOException Si la validation technique du DAO est incorrecte
+	 */
+	public SeasonDAO(Class<Season> daoClass)
+	throws DAOException {
+		super(daoClass);
+	}
+}
