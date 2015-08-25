1f1e4aa462725ba0f2050a3b2a1c768ac7cc1bac
Jérémy TOUZY <j.touzy@qualiac.com>
2015-08-25 13:25:16 +0200

    Lecture du backup depuis Dropbox

--- src/main/java/com/jtouzy/cv/model/tools/api/DropboxAPI.java
+++ src/main/java/com/jtouzy/cv/model/tools/api/DropboxAPI.java
@@ -1,6 +1,7 @@
 package com.jtouzy.cv.model.tools.api;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Locale;
@@ -8,13 +9,37 @@ import java.util.Locale;
 import com.dropbox.core.DbxAppInfo;
 import com.dropbox.core.DbxAuthFinish;
 import com.dropbox.core.DbxClient;
+import com.dropbox.core.DbxEntry;
 import com.dropbox.core.DbxException;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.DbxWebAuthNoRedirect;
 
 public class DropboxAPI {
-	public static void main(String[] args) {
-		DbxClient client = DropboxAPI.getClient("mywypxqmd286kg1", "3xcn95yi2jluokv", "CantalVolley");
+	public static void main(String[] args)
+	throws Exception {
+		//DbxClient client = DropboxAPI.getClient("mywypxqmd286kg1", "3xcn95yi2jluokv", "CantalVolley");
+		DbxClient client = DropboxAPI.getClientByToken("CantalVolley", "lntoAdw3MMQAAAAAAAAF1FKLBpmxtHmOmMq_Ev1mUX1HuiirRS4m1sD8KNkqMc3P");
+		FileOutputStream outputStream = new FileOutputStream("/Users/JTO/Desktop/dbdump.xml");
+		try {
+		    DbxEntry.File downloadedFile = client.getFile("/Développements/CantalVolley/dbdump.xml", null, outputStream);
+		    System.out.println("Metadata: " + downloadedFile.toString());
+		    System.out.println(outputStream.toString());
+		    outputStream.flush();
+		} finally {
+		    outputStream.close();
+		}
+	}
+	
+	public static DbxClient getClientByToken(String requestId, String token) {
+		try {
+			DbxRequestConfig config = new DbxRequestConfig(requestId, Locale.getDefault().toString());
+			DbxClient client = new DbxClient(config, token);
+			System.out.println("Linked account: " + client.getAccountInfo().displayName);
+			return client;
+		} catch (DbxException ex) {
+			ex.printStackTrace();
+			return null;
+		}
 	}
 	
 	public static DbxClient getClient(String dropboxApplicationKey, String secretApplicationKey, String requestId) {
