67abb0db38ffeb4a2e58669ea3a5fe0d5a5ec0d1
Jérémy TOUZY <j.touzy@qualiac.com>
2015-08-28 15:15:11 +0200

    Ajout de la jointure avec l'auteur (utilisateurs)

--- src/main/java/com/jtouzy/cv/model/dao/NewsDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/NewsDAO.java
@@ -1,12 +1,27 @@
 package com.jtouzy.cv.model.dao;
 
+import java.util.List;
+
 import com.jtouzy.cv.model.classes.News;
+import com.jtouzy.cv.model.classes.User;
 import com.jtouzy.dao.errors.DAOException;
+import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.model.TableContextNotFoundException;
 import com.jtouzy.dao.impl.AbstractSingleIdentifierDAO;
 
 public class NewsDAO extends AbstractSingleIdentifierDAO<News> {
 	public NewsDAO(Class<News> daoClass)
 	throws DAOException {
 		super(daoClass);
+	}
+	
+	@Override
+	public List<News> queryAll() throws QueryException {
+		try {
+			return query().directJoin(User.class)
+					      .many();
+		} catch (TableContextNotFoundException ex) {
+			throw new QueryException(ex);
+		}
 	}
 }
