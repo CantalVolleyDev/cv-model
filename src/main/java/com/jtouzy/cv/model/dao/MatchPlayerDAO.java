4a5c3774cccb112a627c4aef3c00b5e1daa14ad2
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-22 14:33:20 +0200

    [New] Recherche des joueurs pour un match

--- src/main/java/com/jtouzy/cv/model/dao/MatchPlayerDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/MatchPlayerDAO.java
@@ -1,12 +1,25 @@
 package com.jtouzy.cv.model.dao;
 
+import java.util.List;
+
 import com.jtouzy.cv.model.classes.MatchPlayer;
 import com.jtouzy.dao.errors.DAOException;
+import com.jtouzy.dao.errors.QueryException;
 import com.jtouzy.dao.impl.AbstractDAO;
+import com.jtouzy.dao.query.Query;
 
 public class MatchPlayerDAO extends AbstractDAO<MatchPlayer> {
 	public MatchPlayerDAO()
 	throws DAOException {
 		super(MatchPlayer.class);
+	}
+	
+	public List<MatchPlayer> getPlayers(Integer matchId)
+	throws QueryException {
+		Query<MatchPlayer> query = query();
+		if (matchId != null) {
+			query.context().addEqualsCriterion(MatchPlayer.MATCH_FIELD, matchId);
+		}
+		return query.many();
 	}
 }
