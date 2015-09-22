55704b0af9f518615b54dac13dd096892e633a32
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-22 23:54:48 +0200

    [New] SeasonTeamPlayer : Jointure avec User par défaut dans query()

--- src/main/java/com/jtouzy/cv/model/dao/SeasonTeamPlayerDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/SeasonTeamPlayerDAO.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.jtouzy.cv.model.classes.SeasonTeamPlayer;
+import com.jtouzy.cv.model.classes.User;
 import com.jtouzy.dao.errors.DAOInstantiationException;
 import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.model.ContextMissingException;
 import com.jtouzy.dao.impl.AbstractDAO;
 import com.jtouzy.dao.query.Query;
 
@@ -13,6 +15,18 @@ public class SeasonTeamPlayerDAO extends AbstractDAO<SeasonTeamPlayer> {
 	public SeasonTeamPlayerDAO() 
 	throws DAOInstantiationException {
 		super(SeasonTeamPlayer.class);
+	}
+	
+	@Override
+	public Query<SeasonTeamPlayer> query() 
+	throws QueryException {
+		try {
+			Query<SeasonTeamPlayer> query = super.query();
+			query.context().addDirectJoin(User.class);
+			return query;
+		} catch (ContextMissingException ex) {
+			throw new QueryException(ex);
+		}
 	}
 	
 	public List<SeasonTeamPlayer> getPlayers(Integer seasonId, Integer teamId)
