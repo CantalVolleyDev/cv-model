6bfe1d3a54f22602a69a5662a238ee0b7b058bf3
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-22 10:46:57 +0200

    [New] Recherche d'un match avec détails

--- src/main/java/com/jtouzy/cv/model/dao/MatchDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/MatchDAO.java
@@ -58,7 +58,40 @@ public class MatchDAO extends AbstractSingleIdentifierDAO<Match> {
 	 */
 	public Match queryOne(Integer id)
 	throws MatchNotFoundException, QueryException {
-		Match match = super.queryForOne(id);
+		return controlNotFound(id, super.queryForOne(id));
+	}
+	
+	/**
+	 * Recherche des informations d'un seul match avec détails
+	 * (Championnat + Compétition)
+	 * @param id Identifiant unique du match
+	 * @return Match correspondant à l'index
+	 * @throws MatchNotFoundException Si le match n'as pas été trouvé
+	 * @throws QueryException Si problème dans l'exécution de la requête
+	 */
+	public Match queryOneWithDetails(Integer id) 
+	throws MatchNotFoundException, QueryException {
+		try {
+			Query<Match> query = query();
+			query.context()
+			     .addDirectJoin(Championship.class)
+			     .addDirectJoin(ModelContext.getTableContext(Competition.class), Championship.TABLE)
+			     .addEqualsCriterion(Match.IDENTIFIER_FIELD, id);
+			return (controlNotFound(id, query.one()));
+		} catch (ContextMissingException ex) {
+			throw new QueryException(ex);
+		}
+	}
+	
+	/**
+	 * Contrôle commun pour tester si match recherché non null
+	 * @param id Identifiant unique du match
+	 * @param match Match recherché par une requête
+	 * @return Le match recherché par une requête
+	 * @throws MatchNotFoundException Si le match passé en paramètre est null
+	 */
+	private Match controlNotFound(Integer id, Match match)
+	throws MatchNotFoundException {
 		if (match == null) {
 			throw new MatchNotFoundException(id);
 		}
