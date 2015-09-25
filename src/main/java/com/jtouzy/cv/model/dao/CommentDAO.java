b7bd2241ae7e71959668627b7c849c7f6b1d1bc6
Jérémy TOUZY <j.touzy@qualiac.com>
2015-09-25 17:13:42 +0200

    [New] Suite/Fin gestion de la soumission d'un score d'un match

--- src/main/java/com/jtouzy/cv/model/dao/CommentDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/CommentDAO.java
@@ -1,12 +1,42 @@
 package com.jtouzy.cv.model.dao;
 
 import com.jtouzy.cv.model.classes.Comment;
+import com.jtouzy.dao.errors.DAOCrudException;
 import com.jtouzy.dao.errors.DAOException;
+import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.model.ContextMissingException;
 import com.jtouzy.dao.impl.AbstractSingleIdentifierDAO;
+import com.jtouzy.dao.query.CUD;
+import com.jtouzy.dao.query.Query;
 
 public class CommentDAO extends AbstractSingleIdentifierDAO<Comment> {
 	public CommentDAO()
 	throws DAOException {
 		super(Comment.class);
+	}
+	
+	public Comment getMatchTeamComment(Integer matchId, Integer teamId)
+	throws QueryException {
+		Query<Comment> query = query();
+		query.context().addEqualsCriterion(Comment.ENTITY_FIELD, Comment.Entity.MAT);
+		query.context().addEqualsCriterion(Comment.ENTITY_VALUE_FIELD, matchId);
+		query.context().addEqualsCriterion(Comment.TEAM_FIELD, teamId);
+		return query.one();
+	}
+	
+	public void deleteMatchComments(Integer matchId, Integer teamId)
+	throws DAOCrudException {
+		try {
+			CUD<Comment> cud = CUD.build(this.connection, this.daoClass);
+			cud.context().addEqualsCriterion(Comment.ENTITY_FIELD, Comment.Entity.MAT);
+			cud.context().addEqualsCriterion(Comment.ENTITY_VALUE_FIELD, matchId);
+			if (teamId != null) {
+				cud.context().addEqualsCriterion(Comment.TEAM_FIELD, teamId);
+			}
+			cud.executeDelete();
+			//FIXME: QueryException??
+		} catch (ContextMissingException | QueryException ex) {
+			throw new DAOCrudException(Comment.class, ex);
+		}
 	}
 }
--- src/main/java/com/jtouzy/cv/model/dao/MatchPlayerDAO.java
+++ src/main/java/com/jtouzy/cv/model/dao/MatchPlayerDAO.java
@@ -3,9 +3,12 @@ package com.jtouzy.cv.model.dao;
 import java.util.List;
 
 import com.jtouzy.cv.model.classes.MatchPlayer;
+import com.jtouzy.dao.errors.DAOCrudException;
 import com.jtouzy.dao.errors.DAOException;
 import com.jtouzy.dao.errors.QueryException;
+import com.jtouzy.dao.errors.model.ContextMissingException;
 import com.jtouzy.dao.impl.AbstractDAO;
+import com.jtouzy.dao.query.CUD;
 import com.jtouzy.dao.query.Query;
 
 public class MatchPlayerDAO extends AbstractDAO<MatchPlayer> {
@@ -21,5 +24,20 @@ public class MatchPlayerDAO extends AbstractDAO<MatchPlayer> {
 			query.context().addEqualsCriterion(MatchPlayer.MATCH_FIELD, matchId);
 		}
 		return query.many();
+	}
+	
+	public void delete(Integer matchId, Integer teamId)
+	throws DAOCrudException {
+		try {
+			CUD<MatchPlayer> cud = CUD.build(this.connection, this.daoClass);
+			if (matchId != null)
+				cud.context().addEqualsCriterion(MatchPlayer.MATCH_FIELD, matchId);
+			if (teamId != null)
+				cud.context().addEqualsCriterion(MatchPlayer.TEAM_FIELD, teamId);
+			cud.executeDelete();
+			//FIXME: QueryException??
+		} catch (ContextMissingException | QueryException ex) {
+			throw new DAOCrudException(MatchPlayer.class, ex);
+		}
 	}
 }
